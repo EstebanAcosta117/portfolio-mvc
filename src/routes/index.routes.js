cddb051dcca954fa61d3fdc469ceff6a176617f3
EstebanAcosta117 <estebanacostas117@gmail.com>
2024-09-24 13:18:26 -0300

    Primer commit

---
+++ src/routes/index.routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const indexController = require('../controllers/indexController');
+
+router.get('/',indexController.index);
+router.get('/about',indexController.about);
+
+module.exports = router
